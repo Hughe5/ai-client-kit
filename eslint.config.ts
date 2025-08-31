20a17c431303b0ce5ae97905b9b51df25268a17f
shichuanzhi <shichuanzhi@baidu.com>
2025-08-31 19:42:24 +0800

    chore: 改 eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -17,6 +17,14 @@ export default defineConfig([
     },
     rules: {
       ...js.configs.recommended.rules,
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_', // 忽略下划线开头的参数
+          varsIgnorePattern: '^_', // 忽略下划线开头的变量
+        },
+      ],
       'object-curly-spacing': ['error', 'never'],
       'prettier/prettier': 'error',
     },
