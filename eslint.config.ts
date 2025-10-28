4f036012b4c369c7fd8415b554e2aeb6c8c4e748
shichuanzhi <shichuanzhi@baidu.com>
2025-10-28 17:44:16 +0800

    chore: 改 eslint ts config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,34 +1,19 @@
-import js from '@eslint/js';
 import globals from 'globals';
 import {defineConfig} from 'eslint/config';
-import prettierPlugin from 'eslint-plugin-prettier';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   ...tseslint.configs.recommended,
   {
-    files: ['**/*.{js,mjs,cjs,ts,tsx}'],
+    files: ['**/*.ts'],
     languageOptions: {
       globals: globals.browser,
-    },
-    plugins: {
-      prettier: prettierPlugin,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_', // 忽略下划线开头的参数
-          varsIgnorePattern: '^_', // 忽略下划线开头的变量
-        },
-      ],
-      'object-curly-spacing': ['error', 'never'],
-      'prettier/prettier': 'error',
+      parserOptions: {
+        project: true,
+      },
     },
   },
   {
-    ignores: ['node_modules', 'dist', 'build', 'coverage'],
+    ignores: ['node_modules/', 'dist/', 'build/', 'coverage/'],
   },
 ]);
